c3808c9c7da2764859b9818b8add3cb2492454b2
Masashi Kishikawa <masashi@cs.wisc.edu>
2014-01-26 15:51:32 -0600

    routing experiment. #2

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ FirstApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'welcome#index'
+  root 'rails/welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
